a6643f25654a77001007222d33c21a05f2c8de13
Cure Git <37978051+curegit@users.noreply.github.com>
2019-10-06 16:51:09 +0900

    Image util

--- modules/utilities.py
+++ modules/utilities.py
@@ -1,6 +1,9 @@
 import os
 import os.path as path
 import inspect
+from PIL import Image
+from numpy import asarray, rint, clip, uint8, float32
+from chainer import Variable
 
 # Wrapper for doing mkdir -p
 def mkdirp(path):
@@ -16,3 +19,16 @@ def filerelpath(relpath):
 	f = inspect.stack()[1].filename
 	d = os.getcwd() if f == "<stdin>" else path.dirname(f)
 	return path.join(d, relpath)
+
+# Load image to return chainer variable
+def load_image(path):
+	img = Image.open(path)
+	array = asarray(img, dtype=uint8).transpose(2, 0, 1).astype(float32) / 255
+	return Variable(array)
+
+# Save chainer variable of 2d array as image
+def save_image(path, variable):
+	variable.to_cpu()
+	array = clip(rint(variable.array * 255), 0, 255).astype(uint8).transpose(1, 2, 0)
+	img = Image.fromarray(array, "RGB")
+	img.save(path)
