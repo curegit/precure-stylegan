05e389094ffee13214defe5485ac26de6120639a
Cure Git <37978051+curegit@users.noreply.github.com>
2019-10-10 15:20:45 +0900

    Update train.py

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-import sys
+from argparse import ArgumentParser
 
 from chainer import optimizers
 from chainer import serializers
@@ -6,52 +6,69 @@ from chainer.iterators import MultiprocessIterator
 import chainer.training
 from chainer.training import extensions, Trainer
 
-from modules.networks import Generator, Discriminator
 from modules.updater import StyleGanUpdater
 from modules.dataset import StyleGanDataset
-from modules.utilities import filepath, filerelpath, save_image
-
-from argparse import ArgumentParser
+from modules.networks import Generator, Discriminator
+from modules.utilities import filepath, save_image
 
 # Parse command line arguments
 parser = ArgumentParser(allow_abbrev=False, description="Style-Based GAN's Trainer")
 parser.add_argument("datadir", metavar="DATA", help="")
-parser.add_argument("-p", "--preload", action="store_true", help="")
-parser.add_argument("-d", "--directory", metavar="DEST", default=".", help="")
+parser.add_argument("-p", "--preload", action="store_true", help="preload all dataset into RAM")
+#parser.add_argument("-d", "--directory", metavar="DEST", default="result", help="")
 parser.add_argument("-g", "--generator", metavar="FILE", help="")
-parser.add_argument("-s", "--stage", type=int, choices=[1, 2, 3, 4, 5, 6, 7, 8, 9], default=1, help="")
-
-parser.add_argument("-b", "--batch", type=int, default=1, help="batch size, affecting memory usage")
+parser.add_argument("-d", "--discriminator", metavar="FILE", help="")
+parser.add_argument("-o", "--optimizers", metavar="FILE", nargs=3, help="optimizers of mapper, generator and discriminator")
+parser.add_argument("-s", "--stage", type=int, choices=[1, 2, 3, 4, 5, 6, 7, 8, 9], default=1, help="growth stage to train")
 parser.add_argument("-z", "--z-size", dest="size", type=int, default=512, help="latent vector (feature vector) size")
 parser.add_argument("-m", "--mlp-depth", dest="mlp", type=int, default=8, help="MLP depth of mapping network")
+parser.add_argument("-b", "--batch", type=int, default=4, help="batch size, affecting memory usage")
+parser.add_argument("-e", "--epoch", type=int, default=1, help="")
+parser.add_argument("-a", "--alpha", type=float, default=0.0, help="")
+parser.add_argument("-t", "--delta", type=float, default=2**-16, help="")
 parser.add_argument("-v", "--device", type=int, default=-1, help="use specified GPU or CPU device")
 args = parser.parse_args()
 
-# ===== settings =====
-directory = filerelpath("/data")
-z_size = 512
-#batches = [1, 16, 16, 16, 16, 16, 14, 6, 3]
-#batch = batches[stage - 1]
-preload = False
-alpha = 0.0
-delta = 0.00005
-device = 0
+# Validate arguments
+batch = max(1, args.batch)
+epoch = max(1, args.epoch)
+alpha = max(0.0, args.alpha)
+delta = max(0.0, args.delta)
+size = max(1, args.size)
+depth = max(1, args.mlp)
+device = max(-1, args.device)
 
-gen = Generator(z_size)
-if gend is not None:
-	print("Load generator")
-	serializers.load_hdf5(gend, gen)
+# Init models
+print("Initializing models")
+generator = Generator(size, depth)
+discriminator = Discriminator()
 
-dis = Discriminator()
-if disd is not None:
-	print("Load discriminator")
-	serializers.load_hdf5(disd, dis)
+# Prepare dataset
+h, w = generator.resolution(args.stage)
+dataset = StyleGanDataset(args.datadir, (w, h), args.preload)
+iterator = MultiprocessIterator(dataset, batch_size=batch, repeat=True, shuffle=True, n_processes=16)
+n = dataset.__len__()
 
+# Print information
+print(f"Epoch: {epoch}, Batch: {batch}, Images: {n}")
+print(f"MLP: {size}x{depth}, Stage: {args.stage} ({w}x{h})")
+print(f"Device: {'CPU' if device < 0 else f'GPU {device}'}")
+
+# Load models
+if args.generator is not None:
+	print("Loading generator")
+	serializers.load_hdf5(args.generator, generator)
+if args.discriminator is not None:
+	print("Loading discriminator")
+	serializers.load_hdf5(args.discriminator, discriminator)
+
+# GPU setting
 if device >= 0:
-	print("Convert to GPU")
-	gen.to_gpu()
-	dis.to_gpu()
+	print("Converting to GPU")
+	generator.to_gpu(device)
+	discriminator.to_gpu(device)
 
+# Init optimizers
 opt_m = optimizers.Adam().setup(gen.mapper)
 if optmap is not None:
 	print("Load mapper optimizer")
@@ -67,9 +84,8 @@ if optdis is not None:
 	print("Load discriminator optimizer")
 	serializers.load_hdf5(optdis, opt_d)
 
-size = gen.resolution(stage)
-#iterator = SerialIterator(StyleGanDataset(directory, size, preload), batch_size=batch, repeat=True, shuffle=True)
-iterator = MultiprocessIterator(StyleGanDataset(directory, size, preload), batch_size=batch, repeat=True, shuffle=True, n_processes=16)
+# Load optimizers
+
 updater = StyleGanUpdater(gen, dis, iterator, {"mapper": opt_m, "generator": opt_g, "discriminator": opt_d}, device, stage, alpha, delta)
 
 trainer = Trainer(updater, (epoch, "epoch"), out='results')
