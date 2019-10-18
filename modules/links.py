0dfe22a2ffcc02f4b15b897ff412288a9b38ac80
Cure Git <37978051+curegit@users.noreply.github.com>
2019-10-18 16:57:54 +0900

    update links

--- modules/links.py
+++ modules/links.py
@@ -7,9 +7,9 @@ from chainer.initializers import Normal
 # Learning rate-equalized FC layer
 class EqualizedLinear(Chain):
 
-	def __init__(self, in_size, out_size=None, initial_bias=None):
+	def __init__(self, in_size, out_size=None, initial_bias=None, gain=sqrt(2)):
 		super().__init__()
-		self.c = sqrt(2 / in_size)
+		self.c = gain * sqrt(1 / in_size)
 		with self.init_scope():
 			self.linear = Linear(in_size, out_size, initialW=Normal(1.0), initial_bias=initial_bias)
 
@@ -19,11 +19,11 @@ class EqualizedLinear(Chain):
 # Learning rate-equalized convolution layer
 class EqualizedConvolution2D(Chain):
 
-	def __init__(self, in_channels, out_channels, ksize=None, stride=1, pad=0, initial_bias=None):
+	def __init__(self, in_channels, out_channels, ksize=None, stride=1, pad=0, nobias=False, initial_bias=None, gain=sqrt(2)):
 		super().__init__()
-		self.c = sqrt(2 / (in_channels * ksize ** 2))
+		self.c = gain * sqrt(1 / (in_channels * ksize ** 2))
 		with self.init_scope():
-			self.conv = Convolution2D(in_channels, out_channels, ksize, stride, pad, initialW=Normal(1.0), initial_bias=initial_bias)
+			self.conv = Convolution2D(in_channels, out_channels, ksize, stride, pad, nobias=nobias, initialW=Normal(1.0), initial_bias=initial_bias)
 
 	def __call__(self, x):
 		return self.conv(self.c * x)
