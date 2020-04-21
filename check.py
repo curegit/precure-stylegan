b85a6ebbda1629f3d880ea3b2c6ebdbdc51fd534
curegit <37978051+curegit@users.noreply.github.com>
2020-04-21 22:26:37 +0900

    More info

--- check.py
+++ check.py
@@ -1,2 +1,6 @@
+from chainer import cuda
 from chainer import print_runtime_info
+
 print_runtime_info()
+print(f"CUDA: {'Available' if cuda.available else 'Not Available'}")
+print(f"cuDNN: {'Available' if cuda.cudnn_enabled else 'Not Available'}")
