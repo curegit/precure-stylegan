908895bf356d77626a6d8824b881c481e1f60809
curegit <37978051+curegit@users.noreply.github.com>
2020-04-25 11:28:20 +0900

    Optimizer switch

--- train.py
+++ train.py
@@ -39,6 +39,9 @@ parser.add_argument("-t", "--delta", type=positive, default=0.00005, help="")
 parser.add_argument("-G", "--gamma", "--l2-batch", dest="gamma", type=ufloat, default=10, help="")
 parser.add_argument("-L", "--lsgan", "--least-squares", action="store_true", help="")
 parser.add_argument("-i", "--style-mixing", metavar="RATE", dest="mix", type=rate, default=0.5, help="")
+parser.add_argument("-S", "--sgd", metavar="LR", type=positive, nargs=3, help="")
+parser.add_argument("-A", "--adam-alphas", metavar="ALPHA", type=positive, nargs=3, default=(0.00001, 0.001, 0.001), help="Adam's coefficients of learning rates of mapper, generator, and discriminator")
+parser.add_argument("-B", "--adam-betas", metavar="BETA", type=rate, nargs=2, default=(0.0, 0.99), help="Adam's exponential decay rates of the 1st and 2nd order moments")
 parser.add_argument("-u", "--print-interval", metavar="ITER", dest="print", type=uint, nargs=2, default=(5, 500), help="")
 parser.add_argument("-l", "--write-interval", metavar="ITER", dest="write", type=uint, nargs=4, default=(1000, 3000, 500, 500), help="")
 parser.add_argument("-v", "--device", "--gpu", metavar="ID", dest="device", type=device, default=-1, help="use specified GPU or CPU device")
@@ -87,9 +90,20 @@ if args.device >= 0:
 
 # Init optimizers
 print("Initializing optimizers")
-mapper_optimizer = optimizers.Adam(alpha=0.00001, beta1=0.0, beta2=0.99, eps=1e-08).setup(generator.mapper)
-generator_optimizer = optimizers.Adam(alpha=0.001, beta1=0.0, beta2=0.99, eps=1e-08).setup(generator.generator)
-discriminator_optimizer = optimizers.Adam(alpha=0.001, beta1=0.0, beta2=0.99, eps=1e-08).setup(discriminator)
+if args.sgd is None:
+	mapper_optimizer = optimizers.Adam(alpha=args.adam_alphas[0], beta1=args.adam_betas[0], beta2=args.adam_betas[1], eps=1e-08).setup(generator.mapper)
+	print(f"Mapper: Adam(alpha: {args.adam_alphas[0]}, beta1: {args.adam_betas[0]}, beta2: {args.adam_betas[1]})")
+	generator_optimizer = optimizers.Adam(alpha=args.adam_alphas[1], beta1=args.adam_betas[0], beta2=args.adam_betas[1], eps=1e-08).setup(generator.generator)
+	print(f"Generator: Adam(alpha: {args.adam_alphas[1]}, beta1: {args.adam_betas[0]}, beta2: {args.adam_betas[1]})")
+	discriminator_optimizer = optimizers.Adam(alpha=args.adam_alphas[2], beta1=args.adam_betas[0], beta2=args.adam_betas[1], eps=1e-08).setup(discriminator)
+	print(f"Discriminator: Adam(alpha: {args.adam_alphas[2]}, beta1: {args.adam_betas[0]}, beta2: {args.adam_betas[1]})")
+else:
+	mapper_optimizer = optimizers.SGD(args.sgd[0]).setup(generator.mapper)
+	print(f"Mapper: SGD(learning rate: {args.sgd[0]})")
+	generator_optimizer = optimizers.SGD(args.sgd[1]).setup(generator.generator)
+	print(f"Generator: SGD(learning rate: {args.sgd[1]})")
+	discriminator_optimizer = optimizers.SGD(args.sgd[2]).setup(discriminator)
+	print(f"Discriminator: SGD(learning rate: {args.sgd[2]})")
 
 # Load optimizers
 if args.optimizers is not None:
