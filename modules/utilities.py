6f148331b1a9771670460fab25cae359cc09232b
curegit <37978051+curegit@users.noreply.github.com>
2021-02-04 21:49:13 +0900

    Clean utilities.py

--- modules/utilities.py
+++ modules/utilities.py
@@ -9,22 +9,22 @@ from numpy import asarray, rint, clip, uint8, float32, save, load
 def eprint(*args, **kwargs):
 	print(*args, file=sys.stderr, **kwargs)
 
-# Wrapper for doing mkdir -p
+# Wrapper function to do `mkdir -p`
 def mkdirp(path):
 	os.makedirs(path, exist_ok=True)
 
-# Build filepath from dirpath, filename and extension
+# Build a file path from a directory path, filename and extension
 def filepath(dirpath, filename, ext):
 	p = os.path.join(dirpath, filename) + os.extsep + ext
 	return os.path.normpath(p)
 
-# Build path relatively from caller's script directory
+# Return a complete path of a path relative from the caller's script file
 def filerelpath(relpath):
 	f = inspect.stack()[1].filename
 	d = os.path.dirname(f)
 	return os.path.join(d, relpath)
 
-# Make alternate file path
+# Return an alternate path if it conflicts
 def altfilepath(path, suffix="+"):
 	while os.path.lexists(path):
 		root, ext = os.path.splitext(path)
@@ -32,25 +32,25 @@ def altfilepath(path, suffix="+"):
 		path = os.path.join(head, tail + suffix) + ext
 	return path
 
-# Load numpy array from binary file
+# Deserialize a Numpy array
 def load_array(path):
 	return load(path)
 
-# Save numpy array in binary file
+# Serialize a Numpy array
 def save_array(array, path):
 	save(path, array)
 
-# Load image to return numpy array
+# Load an image and return it as a Numpy array
 def load_image(path, size=None):
 	img = Image.open(path).convert("RGB")
 	if size is not None: img = img.resize(size, Image.LANCZOS)
 	return asarray(img, dtype=uint8).transpose(2, 0, 1).astype(float32) / 255
 
-# Convert 2d numpy array to Pillow image
+# Convert a Numpy array to a Pillow image
 def array2image(array):
 	array = clip(rint(array * 255), 0, 255).astype(uint8).transpose(1, 2, 0)
 	return Image.fromarray(array, "RGB")
 
-# Save 2d numpy array as image
+# Save a Numpy array as an image
 def save_image(array, path):
 	array2image(array).save(path)
